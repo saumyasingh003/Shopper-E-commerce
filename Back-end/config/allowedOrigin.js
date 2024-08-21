51cd3930cee94994d9685ae09b8eb99b7125eca8
saumyasingh003 <saumyasingh98982@gmail.com>
2024-08-22 00:33:19 +0530

    commit

--- Back-end/config/allowedOrigin.js
+++ Back-end/config/allowedOrigin.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
    
   
     "http://localhost:5173",
+    "http://localhost:3000",
     "https://shopper-admin-murex.vercel.app",
     "https://shopper-frontend-beta.vercel.app"
   ];
