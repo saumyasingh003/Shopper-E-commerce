f5e2c6d286c2cb3ad1804de435b8609ab219ef93
saumyasingh003 <saumyasingh98982@gmail.com>
2024-09-04 17:02:51 +0530

    latest changes

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
-
 const app = express();
 const cors = require("cors");
 const corsOptions ={
   origin:'*', 
-  credentials:true,           
-  optionSuccessStatus:200
 }
 app.use(cors(corsOptions))
 const path = require("path");
